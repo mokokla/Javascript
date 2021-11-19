bcdc5c0c35854f037c28eb4eccd63f3d8f3fdbf8
mokokla <94524895+mokokla@users.noreply.github.com>
2021-11-19 10:34:16 +0100

    Add files via upload

--- JavaScript/Aufagabe 11 Todolist/script.js
+++ JavaScript/Aufagabe 11 Todolist/script.js
@@ -1,25 +1,40 @@
-var task1 = {name="Wand streichen", isDone= true,responsible="Moritz"};
-var task1 = {name="Tv-Halterung anbringen", isDone= true,responsible="Moritz"};
-
-const tasks = ["putzen","tanzen","kochen"];
+const tasks = [];
 printTasklist();
-
+console.log(tasks)
 
 document.getElementById("addtask").addEventListener("click",function(){
-    var newTask= document.getElementById("txtNewtask").value;
-    tasks.push(newTask);
-    printTasklist();
+    addTask();
 });
+
+function addTask(){
+    var taskname= document.getElementById("txtNewtask").value;
+    var taskResponsible= document.getElementById("txtResponsible").value;
+    let task = {name: taskname, responsible: taskResponsible, isDone:false}
+    tasks.push(task);
+    printTasklist();
+}
 
 function printTasklist(){
     document.getElementById("tasklist").innerHTML= getHTMLtasks();
     
 }
+function markTask(element){
+    let index = element.attributes['data-index'].value;
+    let isChecked = element.checked;
+    tasks[index].isDone = isChecked;
+    printTasklist();
+}
 
 function getHTMLtasks(){
     let html = "";
+    let index = 0;
     tasks.forEach(element => {
-        html += "<li>" + element + "</li>";
+        let checked="";
+        if(element.isDone){
+            checked="checked";
+        }
+        html += "<li><input onClick='markTask(this)' name = 'checkbox' data-index='" + index + "'type='checkbox'" + checked + ">" +element.name + "&nbsp;" + "|"  + "&nbsp;"  + "Responsible: "+  element.responsible  + "&nbsp;" + index+ "</li>";
+        index++;
     });
     return html;
 }
