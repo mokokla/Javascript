71b176ef80266cf4963a623be08a5a769f3d088a
mokokla <94524895+mokokla@users.noreply.github.com>
2021-12-15 09:45:51 +0100

    Old Game Hellraiser reworked and Schere Stein Papier continued

---
+++ JavaScript/Aufgabe 12 Schere-Stein-Papier/script.js
@@ -0,0 +1,5 @@
+let img= document.querySelector("img");
+var choosenFighter = document.getElementById("playerInput").value;
+var PlayerImage = document.getElementById("playerImage");
+PlayerImage.sc
+console.log(choosenFighter)
