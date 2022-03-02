e227846fc0fd50b11123385071a2dfc0787cfea1
mokokla <94524895+mokokla@users.noreply.github.com>
2022-03-02 11:35:03 +0100

    React bis 04 Styling weitergemacht

---
+++ script.js
@@ -0,0 +1,10 @@
+
+const fs = require('fs')
+
+try {
+  const data = fs.readFileSync('C:\\Users\\morit\\Documents\\HTL 4.Klasse 2021-22\\BET\\Arbeitsblatt_Bitcoin_4cWI.docx', 'utf8')
+  console.log(data)
+} catch (err) {
+  console.error(err)
+}
+
