f3f957b75c0441a9561970a9fb3b531eab4961fd
mokokla <94524895+mokokla@users.noreply.github.com>
2022-03-17 11:26:20 +0100

    Aufgabe 06 Weitergemacht

---
+++ Vote.js
@@ -0,0 +1,16 @@
+import React, {Component, createContext} from 'react';
+import styles from './Vote.module.css'
+
+class Vote extends Component{
+    state={ }
+    render() {
+        return(
+            <div className={styles.vote}>
+                <h1>john</h1>
+                <h2>votes</h2>
+            </div>
+         );
+    }
+}
+
+export default Vote;
